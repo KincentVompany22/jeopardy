3e5c5c5cf744c96143ad7933747a4b6d3e0efa13
Joel Izzedin <joelizzedin.dev@gmail.com>
2025-08-20 18:04:20 -0400

    Add automatic category naming

--- app.js
+++ app.js
@@ -3,18 +3,18 @@
 User Story / Pseudo Code:
 
 1) User clicks on a question in category (function: handleQuestionClick() )
-    - on click, original display messaging (cached elements: questionDisplay & answerDisplay) are cleared (function: clear () )
+    - on click, original display messaging (cached elements: questionDisplay & answerDisplay) are cleared (function: clear() )
     - handleQuestionClick function searches for the target.id of the question clicked
     - then searches array questionDirectory using the .find() method for any instance where target.id = questionID
     - returns the object within the questionDirectory array where a match is found 
-        - saves that to variable currentQuestion to use for the handleAnswerClick() function
+        - saves that to variable currentQuestion to use for the handleAnswerClick() function later
     - then displays the question property associated with that matched object to the DOM for the user to see (cached element: questionDisplay)
         - submit answer button (cached element: submitAnswer) is enabled to submit
         - question square's style.color changes to signify current question being answered
         - event listener for handleQuestionClick() function is removed so no other question can be selected
     
-2) User submit answer (function: handleAnswerClick() ) 
-    - user types in answer into input field (cached element: answerInput) and clicks submit answer (cached element: submitAnswer)
+2) User submits answer (function: handleAnswerClick() ) 
+    - user types answer into input field (cached element: answerInput) and clicks submit answer (cached element: submitAnswer)
     - if answerInput.value = currentQuestion.answer (currentQuestion variable defined earlier in handleQuestionClick() function)
         - answerDislay.textContent displays correct answer messaging
         - user score is increased by value of question listed in questionDirectory array (currentQuestion.value)
@@ -22,7 +22,7 @@ User Story / Pseudo Code:
         - answerDislay.textContent displays incorrect answer messaging
         - user score is decreased by value of question listed in questionDirectory array 
     - currentQuestion clicked property in questionDirectory is marked as clicked
-    - submit answer button is disabled again until another question is clicked
+    - submit answer button is disabled again until next question is clicked
     - event listener for handleQuestionClick() function is added back so next question can be selected
 
 3) Board is updated (function: updateGame() )
@@ -39,7 +39,7 @@ User Story / Pseudo Code:
     - sets score back to 0
     - sets the currentQuestion selected back to null
     - sets all of the clicked properties in questionDirectory to false
-    - adds back the event listener for handleQuestionClick() function to all question elements
+    - adds back event listener for handleQuestionClick() function to all question elements
     - submit answer button is disabled
     - displays all initial game messaging
         - question display 
@@ -47,7 +47,7 @@ User Story / Pseudo Code:
         - input value & placeholder
         - score display
         - question element textContent and style.color (cached element: originalValuesDisplay)
-            - Since question elements returns a node list, cannot access textContent or style.color of elements directly
+            - since question elements returns node list, cannot access textContent or style.color of elements directly
             - so original question element textContent and style.color properties are pushed into originalValuesDisplay array to be used during init() function
 */
 
@@ -59,6 +59,7 @@ const questionDirectory = [
         // must be added in same format with question & category ID's and values lined up.
         // have to add categoryID and categoryName key / property pairs to existing object or it messes up the ID match in handleQuestionClick
             // only need to add categoryID and categoryName key / property pairs once because of the .find() method used to identify them 
+
     {categoryID: "category1", categoryName: "SNL Celebrity Jeopardy!",
      questionID: "column1-question1", clicked: false, value: "$100", question: "This barnyard animal says 'Moo.'", answer: "What is a cow?"},
     {questionID: "column1-question2", clicked: false, value: "$200", question: "It comes out of your faucet, and you shower in it too.", answer: "What is water?"},
@@ -72,6 +73,14 @@ const questionDirectory = [
     {questionID: "column2-question3", clicked: false, value: "$300", question: "These legumes are used to make hummus.", answer: "What are chickpeas?"},
     {questionID: "column2-question4", clicked: false, value: "$400", question: "This cocktail of vodka, tomato juice, and spices is often enjoyed at brunch.", answer: "What is a Bloody Mary?"},
     {questionID: "column2-question5", clicked: false, value: "$500", question: "This spice, sometimes called “red gold,” is the most expensive in the world by weight and comes from the stigma of a flower.", answer: "What is saffron?"},
+
+    {categoryID: "category3", categoryName: "Around the World!",
+     questionID: "column3-question1", clicked: false, value: "$100", question: "The pyramids of Giza are found in this country.", answer: "What is Egypt?"},
+    {questionID: "column3-question2", clicked: false, value: "$200", question: "This U.S. state is famous for having the Grand Canyon", answer: "What is Arizona?"},
+    {questionID: "column3-question3", clicked: false, value: "$300", question: "This country is shaped like a boot.", answer: "What is Italy?"},
+    {questionID: "column3-question4", clicked: false, value: "$400", question: "The Eiffel Tower is located in this European city.", answer: "What is Paris?"},
+    {questionID: "column3-question5", clicked: false, value: "$500", question: "This city is the capital of Canada.", answer: "What is Ottawa?"},
+
 ]
 
 /*-------------------------------- Variables --------------------------------*/
@@ -82,21 +91,36 @@ let currentQuestion
 /*------------------------ Cached Element References ------------------------*/
 
 // Categories--------------------------------------------
-// must repeat steps within Categories section for all categories added
 
-const categoryOne = document.getElementById("category1")
-    // console.dir(categoryOne) // returns element object
-    const categoryOneTitle = questionDirectory.find((question) => { // finds the first instance of "category1"
-        return question.categoryID === "category1"
-    }) 
-    // console.dir(categoryOneTitle) // returns the category1 object to pull category name from
-    categoryOne.textContent = categoryOneTitle.categoryName
-    
-const categoryTwo = document.getElementById("category2")
-    const categoryTwoTitle = questionDirectory.find((question) => { 
-        return question.categoryID === "category2"
+const categoryEls = document.querySelectorAll(".category") // selecting category classes
+let categoryNameArray = [] // create array to populate with categoryNames from questionDirectory        
+
+const pushCategoryArray = () => {
+    questionDirectory.forEach((category) => {
+        categoryNameArray.push(category.categoryName) // push categoryNames from questionDirectory to categoryNameArray
     })
-    categoryTwo.textContent = categoryTwoTitle.categoryName
+}
+
+pushCategoryArray()     
+    // console.dir(categoryNameArray) // checking that categoryNameArray is populating
+    // working, however, pushes undefined values because looking at every object in questionDirectory array for category name when only one per element
+
+const removeUndefined = () => {
+    categoryNameArray = categoryNameArray.filter((category) => { // filter out all undefined elements in categoryNameArray
+        return category
+    })
+}
+
+removeUndefined()
+    // console.dir(categoryNameArray) // checking that categoryNameArray now only has valid elements
+
+const assignCategoryName = () => {
+    categoryEls.forEach((category,index) => {
+          return category.textContent = categoryNameArray[index] // add elements in updated categoryNameArray as textContent for category elements
+    })
+}
+
+assignCategoryName()
 
 // Questions--------------------------------------------
 
@@ -138,7 +162,7 @@ const answerDisplay = document.getElementById("answer-display")
 const originalAnswerDisplay = answerDisplay.textContent
     //console.dir(originalAnswerDisplay)
 
-// Score & Reset--------------------------------------------
+// Score, Reset & Results --------------------------------------------
 
 const scoreDisplay = document.getElementById("score-display")
     //console.dir(scoreDisplay)
@@ -146,9 +170,17 @@ const scoreDisplay = document.getElementById("score-display")
 const resetButton = document.getElementById("reset")
     //console.dir(resetButton)
 
+const originalResetButton = resetButton.textContent
+    // console.dir(originalResetButton)
+
+const gameResultHeader = document.querySelector("header")
+    // parent of question, answer and future winner / loser messaging
+
+const gameResultText = document.createElement("p")
+    
 /*-------------------------------- Functions --------------------------------*/
 
-const clear = () => { // properties start empty & everytime a quesiton is clicked become empty
+const clear = () => { // properties start empty & everytime quesiton is clicked become empty
     
     answerDisplay.textContent = "" 
     answerInput.value = "" 
@@ -173,6 +205,7 @@ const handleQuestionClick = (event) => {
 
     let squareClicked = document.getElementById(boardID)
     squareClicked.style.color = "white" // changing question element dislay once clicked to signify question selected
+    
     questionDirectory.forEach((question,index) => { // remove event listener from all question elements so user cannot select another during answer
         questionEls[index].removeEventListener("click",handleQuestionClick)
     })
@@ -194,9 +227,14 @@ const handleSubmit = (event) => {
     currentQuestion.clicked = true 
         // adjusting clicked property in current question object of questionDisplay array 
         // used to determine when the game is over in checkGameResult()
-        // console.dir(currentQuestion) // checking to see if clicked propery updates
+        // console.dir(currentQuestion) // checking to see if clicked property updates
 
-    scoreDisplay.textContent = score
+    if (score < 0) { // score formatting
+        scoreDisplay.textContent = `-$${Math.abs(score)}` // if negative, use Math.abs() method to extract absolute value of score 
+    } else {
+        scoreDisplay.textContent = `$${score}` // if positive, just display score as is
+    }
+
     submitAnswer.disabled = true // ensures user cannot click submit again without selecting next question
 
     questionDirectory.forEach((question,index) => { 
@@ -228,15 +266,17 @@ const checkGameResult = () => {
         return question.clicked === true }) 
         &&
         score > 0) {
-            questionDisplay.textContent = "Congratulations, you are a Jeopardy champion! 🏆"
-            answerDisplay.textContent = `You're going home with $${score}!`
+            gameResultHeader.prepend(gameResultText) // prepend adds element as first chilld of parent
+            gameResultText.textContent = `🏆 Congratulations, you are a Jeopardy champion! You're going home with $${score}!`
+            gameResultText.style.color = "#ffcc00"
             resetButton.textContent = "Play again?"
         
     } else if (questionDirectory.every((question) => {
         return question.clicked === true}) 
         && score < 0) {
-            questionDisplay.textContent = "Sorry, you lost."
-            answerDisplay.textContent = "Click the button below to try again!"
+            gameResultHeader.prepend(gameResultText)
+            gameResultText.textContent = `👎 Sorry, you lost. Click the button at the bottom to play again!`
+            gameResultText.style.color = "#ffcc00"
             resetButton.textContent = "Play again?"
 
     } else {
@@ -250,17 +290,17 @@ const init = () => {
     score = 0
     currentQuestion = null
 
-    submitAnswer.disabled = true
     questionDirectory.forEach((question) => { 
         question.clicked = false
     })
         // console.log("Init works") // confirming init() function works before adding all of display changes
-
+    
+    gameResultText.remove() // using remove() method to remove the game result text
     questionDisplay.textContent = originalQuestionDisplay
     answerDisplay.textContent = originalAnswerDisplay
     answerInput.placeholder = originalInputPlaceholder
     answerInput.value = ""
-    scoreDisplay.textContent = 0
+    submitAnswer.disabled = true
 
     questionEls.forEach((element,index) => { // again forEach can access the properties wihin the questionEls node
         element.textContent = originalValuesDisplay[index].text // cycle through each index of originalValuesDisplay array to add textContent property stored in 'text' key
@@ -271,6 +311,9 @@ const init = () => {
         question.addEventListener("click",handleQuestionClick) // add back event listener to each question element
         
     })
+
+    scoreDisplay.textContent = 0
+    resetButton.textContent = originalResetButton
 
 }
 
@@ -285,6 +328,32 @@ submitAnswer.addEventListener("click",handleSubmit)
 resetButton.addEventListener("click",init)
 
 /*----------------------------- Code Graveyard -----------------------------*/
+
+
+// Old Manual Category Name textContent Assignment Code
+
+/*
+const categoryOne = document.getElementById("category1")
+    // console.dir(categoryOne) // returns element object
+    const categoryOneTitle = questionDirectory.find((question) => { // finds the first instance of "category1"
+        return question.categoryID === "category1"
+    }) 
+    // console.dir(categoryOneTitle) // returns the category1 object to pull category name from
+    categoryOne.textContent = categoryOneTitle.categoryName
+    
+const categoryTwo = document.getElementById("category2")
+    const categoryTwoTitle = questionDirectory.find((question) => { 
+        return question.categoryID === "category2"
+    })
+    categoryTwo.textContent = categoryTwoTitle.categoryName
+
+const categoryThree = document.getElementById("category3")
+    const categoryThreeTitle = questionDirectory.find((question) => { 
+        return question.categoryID === "category3"
+    })
+    categoryThree.textContent = categoryThreeTitle.categoryName
+*/
+
 
 // Attempted Daily Double Wager Functionality
 
